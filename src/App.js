7fa92daa5022e9e0d08f5a39f73a4d03eb5b4318
Amaan Nawab <nawab.mohdamaan13@gmail.com>
2020-09-06 17:06:10 +0530

    Update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'; 
+
+const HatsPage = () => (
+ <div>
+   <h1>
+     HATS PAGE!!!
+   </h1>
+
+ </div>
+)
 
 function App() {
   return (
     <div >
-     <Homepage/>
+      <Switch>
+     <Route  exact  path='/' component = {Homepage}/>
+      <Route  path='/hats' component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,33 +12,33 @@ class Directory extends React.Component {
                   title: 'Hats',
                   imageUrl: 'https://i0.wp.com/gentlemanzone.com/wp-content/uploads/2018/01/guide-hats-min.jpg?fit=1599%2C900&ssl=1',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'Jackets',
                   imageUrl: 'http://4.bp.blogspot.com/-Oal-4URQG_4/Th1JXxAUxtI/AAAAAAAAAnM/bf6EIImYyiA/s1600/zara-homme-hiver-2010-2011-2.jpg',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: ''
                 },
                 {
                   title: 'Sneakers',
                   imageUrl: 'https://media.gqindia.com/wp-content/uploads/2016/05/GQ-India-GUCCI-414684_KW040_8162.jpg',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: ''
                 },
                 {
                   title: 'Womens',
                   imageUrl: 'https://www.interbrand.com/wp-content/uploads/2018/09/QA_Zara_v1.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: ''
                 },
                 {
                   title: 'Mens',
                   imageUrl: 'http://ftape.com/media/wp-content/uploads/2013/08/Zara-FW13-Mens-Campaign_01.jpg',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: ''
                 }
               ],
         }
@@ -50,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
             {
-            this.state.sections.map( ({title , imageUrl ,id ,size}) => (
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+            this.state.sections.map( ({title , imageUrl ,id ,size , linkUrl}) => (
+                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
             ))
             }
                 </div>
