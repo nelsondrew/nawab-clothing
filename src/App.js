04ba667d68b8b06eb568a387c708aa684a91901b
Amaan Nawab <nawab.mohdamaan13@gmail.com>
2020-09-07 01:56:25 +0530

    Added Header and Zara logo to the Website

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'; 
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/Header/header.component';
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
      <Route  exact  path='/' component = {Homepage}/>
       <Route  path='/shop' component = {ShopPage}/>
