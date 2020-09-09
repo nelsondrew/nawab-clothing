2dc10602aeb556e1727f503f371734fbafe36951
Amaan Nawab <nawab.mohdamaan13@gmail.com>
2020-09-09 14:05:23 +0530

    moved our directory and shop data into its own reducer

--- src/components/collections-overview/collections.overview.component.jsx
+++ src/components/collections-overview/collections.overview.component.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
 import './collections-overview.styles.scss';
 
-import { createStructuredSelector} from 'reselect';
-import  CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import{ selectCollections} from '../../redux/shop/shop.selectors';
-
 const CollectionsOverview = ({ collections }) => (
-    <div className='collections-overview'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-const mapStateToProps = createStructuredSelector ({
-    collections : selectCollections
-   });
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
+});
 
-
-export default connect (mapStateToProps)(CollectionsOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
